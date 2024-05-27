5363dea5f77e247f127de74fe843ea539e0a7106
Putin Pavel Alexandrovich <pavelputin2003@yandex.ru>
2024-05-27 07:59:29 +0300

    feat: make mock auth service

--- lib/model/repository/abstract_restaurant_repository.dart
+++ lib/model/repository/abstract_restaurant_repository.dart
@@ -2,6 +2,7 @@ import 'package:restobook_web/model/entities/restaurant.dart';
 
 abstract class AbstractRestaurantRepository {
   Future<List<Restaurant>> getAll();
+  Future<Restaurant> getById(int id);
   Future<Restaurant> create(Restaurant restaurant);
   Future<Restaurant> update(Restaurant restaurant);
   Future<void> delete(Restaurant restaurant);
---
+++ lib/view_model/restaurant_view_model.dart
@@ -0,0 +1,55 @@
+import 'dart:collection';
+
+import 'package:flutter/cupertino.dart';
+import 'package:get_it/get_it.dart';
+import 'package:restobook_web/model/entities/restaurant.dart';
+import 'package:restobook_web/model/repository/abstract_restaurant_repository.dart';
+
+import '../model/entities/employee.dart';
+import '../model/repository/abstract_employee_repository.dart';
+
+class RestaurantViewModel extends ChangeNotifier {
+  AbstractRestaurantRepository restaurantRepository = GetIt.I<AbstractRestaurantRepository>();
+  AbstractEmployeeRepository employeeRepository = GetIt.I<AbstractEmployeeRepository>();
+
+  List<Restaurant> _restaurants = [];
+  UnmodifiableListView<Restaurant> get restaurants => UnmodifiableListView(_restaurants);
+
+  Restaurant? _activeTable;
+  Restaurant? get activeRestaurant => _activeTable;
+  set activeRestaurant(Restaurant? table) => _activeTable = table;
+
+  final List<Employee> _activeRestaurantEmployees = [];
+  UnmodifiableListView<Employee> get activeTableReservations =>
+      UnmodifiableListView(_activeRestaurantEmployees);
+
+  Future<void> load() async {
+    // TODO: ADD HTTP REQUEST TO GET ALL EMPLOYEES
+    _restaurants = await restaurantRepository.getAll();
+    notifyListeners();
+  }
+
+  Future<void> loadActiveRestaurant(int restaurantId) async {
+    // TODO: ADD HTTP REQUEST TO GET EMPLOYEES BY ID
+    activeRestaurant = await restaurantRepository.getById(restaurantId);
+    notifyListeners();
+  }
+
+  Future<void> add(Restaurant restaurant) async {
+    // TODO: ADD HTTP REQUEST TO CREATE EMPLOYEE
+    activeRestaurant = await restaurantRepository.create(restaurant);
+    notifyListeners();
+  }
+
+  Future<void> update(Restaurant restaurant) async {
+    // TODO: ADD HTTP REQUEST TO UPDATE EMPLOYEE
+    activeRestaurant = await restaurantRepository.update(restaurant);
+    notifyListeners();
+  }
+
+  Future<void> delete(Restaurant restaurant) async {
+    // TODO: ADD HTTP REQUEST TO DELETE EMPLOYEE
+    await restaurantRepository.delete(restaurant);
+    notifyListeners();
+  }
+}
