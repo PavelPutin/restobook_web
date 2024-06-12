cd87b30092202e88ca9172ee1f2f77d593ad3c3f
Putin Pavel Alexandrovich <pavelputin2003@yandex.ru>
2024-06-12 09:55:47 +0300

    fix: change vendor admin title

--- lib/main.dart
+++ lib/main.dart
@@ -60,7 +60,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Restobook администрирование',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
